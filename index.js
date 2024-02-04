e9eb7bffdb4f0fbb669edbce819dc7fcfd219f41
Christian Nuss <christian.nuss@gmail.com>
2024-02-04 11:25:32 -0500

    use console.log

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const fs = require("fs");
 
 module.exports.handler = async (event) => {
   const dir = event.dir || "/";
-  console.log("listing files in dir: ", dir);
-  return fs.readdirSync(dir).join("\n");
+  console.log(`[${process.cwd}] listing files in dir: `, dir);
+  console.log(fs.readdirSync(dir).join("\n"));
 };
