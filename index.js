1547cc267055ffaa0676c616fecf4ff41a4569b6
Christian Nuss <christian.nuss@gmail.com>
2024-02-04 11:10:44 -0500

    just listdir

--- index.js
+++ index.js
@@ -1,30 +1,7 @@
-var _getAllFilesFromFolder = function (dir) {
-  var filesystem = require("fs");
-  var results = [];
-
-  filesystem.readdirSync(dir).forEach(function (file) {
-    file = dir + "/" + file;
-    var stat = filesystem.statSync(file);
-
-    if (stat && stat.isDirectory()) {
-      results = results.concat(_getAllFilesFromFolder(file));
-    } else results.push(file);
-  });
-
-  return results;
-};
+const fs = require("fs");
 
 module.exports.handler = async (event) => {
   const dir = event.dir || "/";
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: JSON.stringify(_getAllFilesFromFolder(dir)),
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+  console.log("listing files in dir: ", dir);
+  return fs.readdirSync(dir).join("\n");
 };
