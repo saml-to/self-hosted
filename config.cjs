d823dc9cc56627899caafb983a69cc9ad7a8ce0f
Christian Nuss <christian.nuss@gmail.com>
2024-02-06 07:47:01 -0500

    add auth api

--- config.cjs
+++ config.cjs
@@ -1,7 +1,8 @@
 module.exports.GH_APP_URL = process.env[`GH_APP_URL`];
 module.exports.GH_APP_ID = process.env[`GH_APP_ID`];
 module.exports.GH_CLIENT_ID = process.env[`GH_CLIENT_ID`].toUpperCase();
-module.exports.GH_APP_SECRETS = JSON.stringify({
+
+module.exports.SECRETS = JSON.stringify({
   [`GITHUB_${module.exports.GH_CLIENT_ID.replace(".", "_")}`]:
     process.env[`GH_CLIENT_SECRET`],
   [`GITHUB_CLIENT_ID_${process.env[`GH_APP_ID`]}`]: process.env[`GH_CLIENT_ID`],
@@ -11,3 +12,6 @@ module.exports.GH_APP_SECRETS = JSON.stringify({
   [`GITHUB_WEBHOOK_SECRET_${process.env[`GH_APP_ID`]}`]:
     process.env[`GH_WEBHOOK_SECRET`],
 });
+
+module.exports.AUTH_VERSION = 4;
+module.exports.GITHUB_VERSION = 22;
